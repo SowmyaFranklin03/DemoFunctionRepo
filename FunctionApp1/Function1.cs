c762c671d1febe99d9468917c79f5af4da9d033b
SowmyaFranklin <chatla.sowmya-franklin@capgemini.com>
2026-05-19 11:37:20 +0530

    fixed azure deployment

--- FunctionApp1/Function1.cs
+++ FunctionApp1/Function1.cs
@@ -1,23 +1,26 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System.Net;
 
 namespace FunctionApp1;
 
 public class Function1
 {
-    private readonly ILogger<Function1> _logger;
-
-    public Function1(ILogger<Function1> logger)
+    private readonly ILogger _logger;
+    public Function1(ILoggerFactory loggerFactory)
     {
-        _logger = logger;
+        _logger = loggerFactory.CreateLogger<Function1>();
     }
-
     [Function("Function1")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    public HttpResponseData Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
+        _logger.LogInformation("Function triggered.");
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.WriteString("Welcome to Azure Functions!");
+        return response;
     }
 }
